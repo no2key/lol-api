22a90216183fbd91b4121414754adae65818bd4d
yuguo <fishguo321@gmail.com>
2012-12-17 18:48:44 +0800

    first commit

---
+++ demo.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+
+</head>
+<body>
+
+<?php
+
+    $url = "http://yuguo.us";
+
+    $str = curl($url);
+    echo $str;
+
+ function curl($url, $postFields = null)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_FAILONERROR, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        //proxy
+        curl_setopt($ch, CURLOPT_PROXY, "http://proxy.tencent.com:8080");
+		//https 请求
+		if(strlen($url) > 5 && strtolower(substr($url,0,5)) == "https" ) {
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		}
+
+		if (is_array($postFields) && 0 < count($postFields))
+		{
+			$postBodyString = "";
+			$postMultipart = false;
+			foreach ($postFields as $k => $v)
+			{
+				if("@" != substr($v, 0, 1))//判断是不是文件上传
+				{
+					$postBodyString .= "$k=" . urlencode($v) . "&";
+				}
+				else//文件上传用multipart/form-data，否则用www-form-urlencoded
+				{
+					$postMultipart = true;
+				}
+			}
+			unset($k, $v);
+			curl_setopt($ch, CURLOPT_POST, true);
+			if ($postMultipart)
+			{
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+			}
+			else
+			{
+				curl_setopt($ch, CURLOPT_POSTFIELDS, substr($postBodyString,0,-1));
+			}
+		}
+		$reponse = curl_exec($ch);
+
+		if (curl_errno($ch))
+		{
+			throw new Exception(curl_error($ch),0);
+		}
+		else
+		{
+			$httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			if (200 !== $httpStatusCode)
+			{
+				throw new Exception($reponse,$httpStatusCode);
+			}
+		}
+		curl_close($ch);
+		return $reponse;
+	}
+?>
+
+</body>
+</html>
---
+++ fetch/hero_list.php
@@ -0,0 +1,36 @@
+<?php include('simple_html_dom.php'); ?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<?php
+function fetchDuowan($htmlstring){
+
+    // get DOM from URL or file
+    $html = file_get_html($htmlstring);
+
+    foreach($html->find('#champion_list li') as $element) {
+
+       print_r($element->children(0)->href);
+       echo "<img src='".$element->children(0)->children(0)->src."'>";
+       print_r($element->children(1)->plaintext);
+    }
+
+    //return $data;
+}
+
+    $duowan = fetchDuowan("http://lol.duowan.com/s/heroes.html");
+
+    //$resultString = json_encode($duowan);
+
+
+
+    //file_put_contents('hero/heroList.json', $resultString);
+
+
+?>
+
+</body>
+</html>
